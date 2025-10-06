af5491af46347d7adceba398a972d4d278f23913
DegrassiAaron <badsworm@gmail.com>
2025-10-06 19:13:54 +0200

    fix dockerfile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // Enable Docker-optimized output
+};
 module.exports = nextConfig;
